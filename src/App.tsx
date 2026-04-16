d010defdccad7bd29acc141c9d324663aee03248
Alex <al618ex@gmail.com>
2026-04-15 21:25:55 -0500

    Genesis

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+import { Todos } from "./assets/components/Todos"
+
+const mockTodos = [
+  {
+    id: 1,
+    text: "tarea 1",
+    completed: false
+  },
+  {
+    id: 2,
+    text: "tarea 2",
+    completed: false
+  },
+  {
+    id: 3,
+    text: "tarea 3",
+    completed: false
+  }
+]
+
+
+const App = () => {
+  const [todos, setTodos] = useState(mockTodos)
+
+  return (
+    <Todos todos={todos} />
+  )
+}
+
+export default App
---
+++ src/assets/components/Todos.tsx
@@ -0,0 +1,26 @@
+import type { Todo } from "../App";
+
+type Todo = {
+    id: number;
+    text: string;
+    completed: boolean;
+}
+
+type Props = {
+    todos: Todo[];
+};
+
+export function Todos({ todos }: Props) {
+    return (
+        <ul>
+            {todos.map((todo) => (
+                <li key={todo.id}>
+                    <label>
+                        <input type="checkbox" checked={todo.completed} readOnly />
+                        {todo.text}
+                    </label>
+                </li>
+            ))}
+        </ul>
+    );
+}
