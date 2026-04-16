9b4555f19056595a44f585ff292452f0b01d07e2
Alex <al618ex@gmail.com>
2026-04-15 23:12:38 -0500

    primercommit

--- src/assets/components/Todos.tsx
+++ src/assets/components/Todos.tsx
@@ -1,8 +1,8 @@
 
 type Todo = {
-    id: number;
-    text: string;
-    completed: boolean;
+    id?: number;
+    text?: string;
+    completed?: boolean;
 }
 
 type Props = {
@@ -14,6 +14,10 @@ export function Todos({ todos }: Props) {
         <ul>
             {todos.map((todo) => (
                 <li key={todo.id}>
+                    <label>
+                        <input type="checkbox" checked={todo.completed} readOnly />
+                        {todo.text}
+                    </label>
                 </li>
             ))}
         </ul>
