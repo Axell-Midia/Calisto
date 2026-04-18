d00090bf60efb9bbdfdfa5467325beb955e32dae
Alex <al618ex@gmail.com>
2026-04-17 22:39:54 -0500

    conflicto solucionado

--- src/assets/components/Todos.tsx
+++ src/assets/components/Todos.tsx
@@ -1,15 +1,8 @@
 type Todo = {
-<<<<<<< HEAD
-    id: number;
-    text: string;
-    completed: boolean;
-}
-=======
-  id?: number;
-  text?: string;
-  completed?: boolean;
+  id: number;
+  text: string;
+  completed: boolean;
 };
->>>>>>> ganimedes
 
 type Props = {
   todos: Todo[];
