e334f2a40ccbd848324433c8a51b18f81f60db88
Alex <al618ex@gmail.com>
2026-04-17 22:33:13 -0500

    correcion

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,33 @@
-import { useState } from "react"
-import { Todos } from "./assets/components/Todos"
+import { useState } from "react";
+import { Todos } from "./assets/components/Todos";
 
 const mockTodos = [
   {
     id: 1,
     text: "tarea 1",
-    completed: false
+    completed: false,
   },
   {
     id: 2,
     text: "tarea 2",
-    completed: false
+    completed: false,
   },
   {
     id: 3,
     text: "tarea 3",
-    completed: false
-  }
+    completed: false,
+  },
   {
     id: 4,
     text: "tarea 4",
-    completed: false
-  }
-]
-
+    completed: false,
+  },
+];
 
 const App = () => {
-  const [todos, setTodos] = useState(mockTodos)
+  const [todos, setTodos] = useState(mockTodos);
 
-  return (
-    <Todos todos={todos} />
-  )
-}
+  return <Todos todos={todos} />;
+};
 
-export default App
+export default App;
