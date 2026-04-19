2bde60d350e29c86e899441f74d6152d3f4f08f6
Alex <al618ex@gmail.com>
2026-04-18 22:43:06 -0500

    buttonVScontador

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
 import MyButton from "./assets/components/Button"
 import Contador from "./assets/components/Contador"
-import { useState } from "react"
 
 const App = () => {
-  const [valor, setValor] = useState(0)
-
-  const sumar = () => {
-    setValor(valor + 1)
-  }
-
-  const restar = () => {
-    setValor(valor - 1)
-  }
-
-  const reset = () => {
-    setValor(0)
-  }
 
   return (
     <>
-      <MyButton variant="primary" c={() => alert("login")} >
-        Acceder
-      </MyButton>
-      <MyButton variant="secondary">
-        Registrarse
-      </MyButton>
-      <MyButton variant="danger" c={() => alert("delete")}>
-        Eliminar
-      </MyButton>
-      <Contador sumar={sumar} restar={restar} reset={reset} valor={valor} />
+      <div className="bg-black text-white min-h-screen">
+
+        <MyButton variant="primary" c={() => alert("login")} >
+          Acceder
+        </MyButton>
+        <MyButton variant="secondary">
+          Registrarse
+        </MyButton>
+        <MyButton variant="danger" c={() => alert("delete")}>
+          Eliminar
+        </MyButton>
+        <Contador />
+      </div>
     </>
   )
 }
--- src/assets/components/Contador.tsx
+++ src/assets/components/Contador.tsx
@@ -1,20 +1,39 @@
-type ContadorProps = {
-    sumar: () => void
-    restar: () => void
-    reset: () => void
-    valor: number
-}
+import { useState } from "react"
 
-function Contador({ sumar, restar, valor, reset }: ContadorProps) {
+const Contador = () => {
+    const [valor, setValor] = useState(0)
+
+    const sumar = () => {
+        setValor(valor + 1)
+    }
+
+    const restar = () => {
+        setValor(valor - 1)
+    }
+
+    const reset = () => {
+        setValor(0)
+    }
+
+    const potencia = () => {
+        setValor(valor * valor)
+    }
+
     return (
         <>
             <div className="flex flex-col items-center justify-center">
                 <p className="text-2xl font-bold">{valor}</p>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={sumar}>Sumar</button>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={restar}>Restar</button>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={reset}>Reiniciar</button>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={sumar}>Sumar</button>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={restar}>Restar</button>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={reset}>Reiniciar</button>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={potencia}>Potencia</button>
             </div>
         </>
     )
+
 }
 export default Contador
